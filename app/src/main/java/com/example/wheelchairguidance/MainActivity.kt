dd2291e76fedd66cb29e3f4fa637396c1604c4f9
Raghavendra J <89856193+Raghavj2000@users.noreply.github.com>
2022-05-21 19:39:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/wheelchairguidance/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.wheelchairguidance
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.wheelchairguidance.login.LoginActivity
+import com.example.wheelchairguidance.login.SignupActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var logoutButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        logoutButton = findViewById(R.id.logoutButton)
+
+        logoutButton.setOnClickListener { logoutUser() }
+
+
+        val intent = Intent(this, MapsActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun logoutUser() {
+        FirebaseAuth.getInstance().signOut();
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
